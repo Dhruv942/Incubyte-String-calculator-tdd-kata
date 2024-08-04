636c3f607f085205fb1cf48674a78fd656782eac
Dhruv Patel <dhruvpatel242231@gmail.com>
2024-08-04 12:06:50 +0530

    Implemen Handle Single Number

--- Stringcal.js
+++ Stringcal.js
@@ -1,7 +1,7 @@
 class StringCalculator {
   add(numbers) {
     if (numbers === "") return 0;
-    return parseInt(numbers, 10);
+    if (!isNaN(numbers)) return parseInt(numbers, 10);
   }
 }
 
--- stringcal.test.js
+++ stringcal.test.js
@@ -1,6 +1,11 @@
-const StringCalculator = require("./StringCalculator");
+const StringCalculator = require("./Stringcal");
 
 test("empty string should return 0", () => {
   const calculator = new StringCalculator();
   expect(calculator.add("")).toBe(0);
+});
+
+test("single number should return the number itself", () => {
+  const calculator = new StringCalculator();
+  expect(calculator.add("1")).toBe(1);
 });
