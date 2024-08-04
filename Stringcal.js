ac81e0a1ae19b84c48c82942e8ec5954bf224cf3
Dhruv Patel <dhruvpatel242231@gmail.com>
2024-08-04 14:43:03 +0530

    Implement  Custom separator

--- Stringcal.js
+++ Stringcal.js
@@ -1,9 +1,41 @@
 class StringCalculator {
   add(expression) {
     if (!expression) return 0;
-    var pieces = expression.split(/[\n,]+/);
-    var sum = 0;
-    for (var i = 0; i < pieces.length; i++) {
+
+    if (expression.startsWith("//")) {
+      const delimiterEndIndex = expression.indexOf("\n");
+      const customDelimiter = expression.substring(2, delimiterEndIndex);
+
+      expression = expression.substring(delimiterEndIndex + 1);
+
+      return this.calculateSum(
+        this.getPieces(expression, [customDelimiter, ",", "\n"])
+      );
+    }
+
+    return this.calculateSum(this.getPieces(expression, [",", "\n"]));
+  }
+
+  getPieces(expression, delimiters) {
+    return this.getSubpieces([expression], delimiters);
+  }
+
+  getSubpieces(far, delimiters) {
+    if (delimiters.length === 0) {
+      return far.flatMap((piece) => piece.split(delimiters[0]));
+    }
+
+    const delimiter = delimiters.pop();
+    let subpieces = [];
+    for (let i = 0; i < far.length; i++) {
+      subpieces = subpieces.concat(far[i].split(delimiter));
+    }
+    return this.getSubpieces(subpieces, delimiters);
+  }
+
+  calculateSum(pieces) {
+    let sum = 0;
+    for (let i = 0; i < pieces.length; i++) {
       sum += parseInt(pieces[i] || 0);
     }
     return sum;
--- stringcal.test.js
+++ stringcal.test.js
@@ -1,13 +1,30 @@
 const StringCalculator = require("./Stringcal");
 const calculator = new StringCalculator();
 
-function checkResult(expression, expected) {
-  test(`should evaluate "${expression}" to ${expected}`, () => {
-    expect(calculator.add(expression)).toBe(expected);
-  });
-}
+describe("StringCalculator", () => {
+  function checkResult(expression, expected) {
+    test(`should evaluate "${expression}" to ${expected}`, () => {
+      expect(calculator.add(expression)).toBe(expected);
+    });
+  }
 
-checkResult("", 0);
-checkResult("42", 42);
-checkResult("1,2", 3);
-checkResult("1,2\n", 3);
+  describe("basic functionality", () => {
+    checkResult("", 0);
+  });
+  describe("Handling single number", () => {
+    checkResult("2", 2);
+  });
+
+  describe("comma separator", () => {
+    checkResult("1,2", 3);
+  });
+
+  describe("comma-separator and newline handling", () => {
+    checkResult("1,2\n", 3);
+    checkResult("1,2\n,3\n,4\n", 10);
+  });
+
+  describe("custom separator", () => {
+    checkResult("//;\n1,2", 3);
+  });
+});
